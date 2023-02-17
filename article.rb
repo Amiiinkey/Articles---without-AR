1de6ac8a4570319f24d5a4401016b426f4022b6e
Mohamed <aminito432@gmail.com>
2023-02-17 20:55:10 +0300

    created article,author,magazine and run files and added the pseudo code

---
+++ article.rb
@@ -0,0 +1,9 @@
+# create an article class
+# should have attributes author, magazine and title
+# initialize attr_reader method to create a getter method for those attributes
+# create an "all" class variable to store all instances of "article" that have been created(should be an empty string)
+# create an initialize method that takes author, magazine and title as arguments and sets them accordingly
+# add the newly created instance to "all" array
+# create a self.all class method that should return an array of all "Article" instances created
+
+
