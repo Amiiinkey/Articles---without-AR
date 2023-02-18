26b15833b5c9dc9b50c182a8f9fac6bf7eef9864
Mohamed <aminito432@gmail.com>
2023-02-18 10:50:11 +0300

    added an @@all and set it to an empty array

--- author.rb
+++ author.rb
@@ -14,6 +14,15 @@ require_relative "./magazine.rb"
 
 class Author
     attr_reader :name 
-    
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
 
+    def self.all
+        @@all 
+    end
+
+    
 end
